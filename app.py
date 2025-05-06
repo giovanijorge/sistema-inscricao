bedb4376c219446c69a548c9d55695642e2d911f
giovanijorge <giovanijorge@gmail.com>
2025-05-06 21:17:41 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
-# Aplicação principal Flask (executável no Render)
+from flask import Flask, render_template
 import os
 
-port = int(os.environ.get("PORT", 10000))
-app.run(host="0.0.0.0", port=port)
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
