81e81ed07bd37971113515109262148ea6a14eab
giovanijorge <giovanijorge@gmail.com>
2025-05-07 01:25:00 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -1,12 +1,7 @@
-from flask import Flask, render_template
-import os
+from flask import Flask
+from sistema_inscricao_pg import create_app
 
-app = Flask(__name__)
+app = create_app()
 
-@app.route("/")
-def index():
-    return render_template("index.html")
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
+if __name__ == '__main__':
+    app.run(debug=True)
