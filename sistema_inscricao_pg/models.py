3b66fa3f4a318f9b2de0e72d0c36486b0f8d1555
giovanijorge <giovanijorge@gmail.com>
2025-05-07 15:48:07 +0100

    Update models.py

--- sistema_inscricao_pg/models.py
+++ sistema_inscricao_pg/models.py
@@ -7,3 +7,10 @@ class Inscricao(db.Model):
     email = db.Column(db.String(120), nullable=False)
     telefone = db.Column(db.String(20), nullable=False)
     data_inscricao = db.Column(db.DateTime, default=datetime.utcnow)
+
+class Admin(db.Model):
+    __tablename__ = 'admins'
+
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    senha = db.Column(db.String(200), nullable=False)  # hash da senha
