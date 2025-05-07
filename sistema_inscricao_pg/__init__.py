0db66f2f6c25ceedfde7791cb9d47fcec925fb3f
giovanijorge <giovanijorge@gmail.com>
2025-05-07 14:33:47 +0100

    Update __init__.py

--- sistema_inscricao_pg/__init__.py
+++ sistema_inscricao_pg/__init__.py
@@ -10,6 +10,9 @@ def create_app():
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://admin:Tf8TM3VaBH2RrYgFEjTFMNWm85PHNViZ@dpg-d0d74p1r0fns73848j80-a/sistema_inscricao'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.secret_key = 'senha_padrao'
+    from sistema_inscricao_pg.admin.views import admin_bp
+app.register_blueprint(admin_bp)
+
 
     db.init_app(app)
     migrate.init_app(app, db)
